93a54d872dc342e1943983e36bbeeeab5b0ccbcd
Davin Chang <davinchang12@gmail.com>
2021-06-25 13:37:29 +0700

    Changed File Organization

--- file_organizer.py
+++ file_organizer.py
@@ -3,30 +3,23 @@ import os
 import shutil
 
 class FileOrganization():
-	# Initialize
-	def __init__(self, current_dir, destination_dir = '', ext):
-		# Current directory that want to be organized
-		self.current_dir = current_dir
-		# Extension that want to be organized
-		self.ext = ext
-		# Destination directory to store the organized extension(s)
-		if destination_dir != '' :
-			self.destination_dir = destination_dir
-
-	def organize(self):
-
+	def organize(self, current_dir, destination_dir, ext):
 		# Search for all file in the current directory
-		for file in os.listdir(self.current_dir):
+		for file in os.listdir(current_dir):
 			# Get file name and file extension from each file in the current directory
 			filename, file_ext = os.path.splitext(file)
 
 			# Check if destination directory is exists
-			if not os.path.exists(self.destination_dir):
-				os.makedirs(self.destination_dir)
+			if not os.path.exists(destination_dir):
+				os.makedirs(destination_dir)
 
 			# Move the file using shutil from current directory to destination directory
-			if file_ext in (self.ext):
+			if file_ext in (ext):
 				shutil.move(
-					os.path.join(self.current_dir, (filename + file_ext)),
-					os.path.join(self.destination_dir,  (filename + file_ext))
+					os.path.join(current_dir, (filename + file_ext)),
+					os.path.join(destination_dir,  (filename + file_ext))
 				)					
+
+	def getExtension(self, current_dir):
+		# Return all available extensions in the folder
+		return list(set([os.path.splitext(file)[1] for file in os.listdir(current_dir) if os.path.splitext(file)[1] != '']))
